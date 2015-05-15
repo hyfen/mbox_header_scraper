6213a931ba8dd6264118e571ce2a7509f94c73ec
Yuya TAMANO <everfree.main@gmail.com>
2015-05-15 17:50:29 +0900

    Add test code for MboxHeaderScraper::Scraper.check_XX_file

--- spec/mbox_header_scraper_spec.rb
+++ spec/mbox_header_scraper_spec.rb
@@ -6,7 +6,28 @@ describe MboxHeaderScraper do
   end
 
   describe MboxHeaderScraper::Scraper do
-    pending '.check_in_file checks if the file is suitable to process.'
-    pending '.check_out_file checks if the file can be created as output file.'
+    describe '.check_in_file' do
+      it 'returns error message if file does not exists' do
+        allow(File).to receive(:exist?).and_return(false)
+        expect(MboxHeaderScraper::Scraper.check_in_file('sample')).to eq('file does not exists.')
+      end
+
+      it 'returns nil if file exists' do
+        allow(File).to receive(:exist?).and_return(true)
+        expect(MboxHeaderScraper::Scraper.check_in_file('sample')).to be_nil
+      end
+    end
+
+    describe '.check_out_file' do
+      it 'returns nil if file does not exists' do
+        allow(File).to receive(:exist?).and_return(false)
+        expect(MboxHeaderScraper::Scraper.check_out_file('sample')).to be_nil
+      end
+
+      it 'returns error message if file exists' do
+        allow(File).to receive(:exist?).and_return(true)
+        expect(MboxHeaderScraper::Scraper.check_out_file('sample')).to eq('file already exists.')
+      end
+    end
   end
 end
