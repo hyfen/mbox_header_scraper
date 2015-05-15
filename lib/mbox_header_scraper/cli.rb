342815de280101718f58e276fe851e3af5b1e717
Yuya TAMANO <everfree.main@gmail.com>
2015-05-15 14:10:41 +0900

    Define command interface.

--- lib/mbox_header_scraper/cli.rb
+++ lib/mbox_header_scraper/cli.rb
@@ -1,5 +1,11 @@
 # Command Interface
-module MboxHeaderScraper
-  class CLI < Thor
+class MboxHeaderScraper::CLI < Thor
+  desc \
+    'mail_address_list {in file} {out file}',
+    'load {in file} and output email address included to {out file}'
+  long_desc \
+    'load mbox file and output in tsv format file. ' \
+    'the file includes No, Subject(short), From, To, CC, Body(email address only)'
+  def mail_address_list(in_file, out_file)
   end
 end
