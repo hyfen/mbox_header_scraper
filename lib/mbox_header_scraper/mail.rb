18c8a2152ef8baa96f51451b59c1efe978231150
Yuya TAMANO <everfree.main@gmail.com>
2015-05-16 17:02:41 +0900

    Add mail gem to read mail file

--- lib/mbox_header_scraper/mail.rb
+++ lib/mbox_header_scraper/mail.rb
@@ -1,11 +1,11 @@
-#require 'mail'
+require 'mail'
 
 # mail object
 class MboxHeaderScraper::Mail
   @mail = nil
 
   def initialize(mail_fp)
-    #@mail = Mail.new(mail_fp)
+    @mail = Mail.read(mail_fp)
   end
 
   def analyze_single_mail(options)
