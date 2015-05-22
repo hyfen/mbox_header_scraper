d67066c256ec50aa74c9d1bd5a6e7dbb6b88e8c6
Yuya TAMANO <everfree.main@gmail.com>
2015-05-22 14:32:42 +0900

    Add test code for Mail.extract_email_address

--- spec/mbox_header_scraper/mail_spec.rb
+++ spec/mbox_header_scraper/mail_spec.rb
@@ -44,4 +44,32 @@ describe MboxHeaderScraper::Mail do
     it 'returns tsv format string with file attached mail file'
     it 'returns empty string with mail file if nothing discovered'
   end
+
+  describe '.extract_email_address' do
+    before :all do
+      @mail = MboxHeaderScraper::Mail.new('')
+
+    end
+    it 'returns mail address for normal email address' do
+      expect(@mail.send(:extract_email_address, 'hogehoge@example.net')).to eq(['hogehoge@example.net'])
+      expect(@mail.send(:extract_email_address, '<hogehoge@example.net>')).to eq(['hogehoge@example.net'])
+      expect(@mail.send(:extract_email_address, 'テスト太郎 <hogehoge@example.net>')).to eq(['hogehoge@example.net'])
+    end
+
+    it 'returns mail address for email address with hyphen' do
+      expect(@mail.send(:extract_email_address, 'hoge-hoge@ex-ample.net')).to eq(['hoge-hoge@ex-ample.net'])
+    end
+
+    it 'returns mail address for email address with underscore' do
+      expect(@mail.send(:extract_email_address, 'hoge_hoge@ex_ample.net')).to eq(['hoge_hoge@ex_ample.net'])
+    end
+
+    it 'returns all mail addresses for multiple email address' do
+      expect(@mail.send(:extract_email_address, 'hoge@example.net,fuga@test.com')).to eq(['hoge@example.net','fuga@test.com'])
+    end
+
+    it 'returns empty string for invalid email address' do
+      expect(@mail.send(:extract_email_address, 'abcdefghijk')).to eq([])
+    end
+  end
 end
